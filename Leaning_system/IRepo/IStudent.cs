e28724c1cdae0e188bcfa9070ccbc8215ea769d8
nhutle8420 <nhutle8420@gmail.com>
2022-06-13 00:30:26 +0700

    Student

---
+++ Leaning_system/IRepo/IStudent.cs
@@ -0,0 +1,13 @@
+﻿using Leaning_system.Models;
+
+namespace Leaning_system.IRepo
+{
+    public interface IStudent
+    {
+        IEnumerable<Student> getAll();
+        Student Get(string usernameHV);
+        public void Delete(string usernameHV);
+        public void Update(Student student);
+        public void Create(Student student);
+    }
+}
---
+++ Leaning_system/IRepo/StudentRepo.cs
@@ -0,0 +1,48 @@
+﻿using Leaning_system.DBContext;
+using Leaning_system.Models;
+
+namespace Leaning_system.IRepo
+{
+    public class StudentRepo : IStudent
+    {
+        private readonly DataContext _dataContext;
+
+
+
+        public StudentRepo(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void Create(Student student)
+        {
+            _dataContext.Students.Add(student);
+            _dataContext.SaveChanges();
+        }
+
+        public void Delete(string usernameHV)
+        {
+            var st = _dataContext.Students.FirstOrDefault(x => x.usernameHV == usernameHV);
+
+
+            _dataContext.Students.Remove(st);
+            _dataContext.SaveChanges();
+
+        }
+
+        public Student Get(string usernameHV)
+        {
+           return _dataContext.Students.FirstOrDefault(x => x.usernameHV == usernameHV);
+        }
+
+        public IEnumerable<Student> getAll()
+        {
+            return _dataContext.Students.ToList();
+        }
+
+        public void Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
