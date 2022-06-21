d33397c0b7aabd1556ce57bd22980c883d79b0db
nhutle8420 <nhutle8420@gmail.com>
2022-06-21 17:49:15 +0700

    add Controller

---
+++ Leaning_system/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Leaning_system.IRepo.Interface;
+using Leaning_system.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leaning_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccount _iaccount;
+        public AccountController(IAccount iaccount) 
+         {
+            _iaccount = iaccount;
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var c = _iaccount.GetAll();
+            if (c == null)
+            {
+                return NotFound();
+            }
+            return Ok(c);
+        }
+        [HttpGet("{UserName}")]
+        public ActionResult GetID(string UserName)
+        {
+            var c = _iaccount.Get(UserName);
+            if (c == null)
+            {
+                return NotFound();
+            }
+            return Ok(c);
+        }
+        [HttpPost]
+        public ActionResult Post(Account c)
+        {
+            _iaccount.Create(c);
+            return Ok();
+        }
+        [HttpDelete("{UserName}")]
+        public ActionResult Delete(string UserName)
+        {
+            if (_iaccount.Delete(UserName))
+                return NoContent();
+            else
+                return BadRequest();
+        }
+        [HttpPut]
+        public ActionResult Put(Account c)
+        {
+
+            _iaccount.Update(c);
+            return NoContent();
+        }
+    }
+}
---
+++ Leaning_system/Controllers/DoucmentController.cs
@@ -0,0 +1,57 @@
+﻿using Leaning_system.IRepo.Interface;
+using Leaning_system.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leaning_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoucmentController : ControllerBase
+    {
+        public readonly IDocument _idocument;
+        public DoucmentController(IDocument document)
+        {
+            _idocument = document;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var t = _idocument.getAll();
+            if (t == null)
+            {
+                return NotFound();
+            }
+            return Ok(t);
+        }
+        [HttpGet("{MaDC}")]
+        public ActionResult GetPower(int MaDC)
+        {
+            var power = _idocument.Get(MaDC);
+            if (power == null)
+            {
+                return NotFound();
+            }
+            return Ok(power);
+        }
+        [HttpPost]
+        public ActionResult PostPower(Documentz documentz)
+        {
+            _idocument.Create(documentz);
+            return Ok();
+
+        }
+        [HttpDelete("{MaDC}")]
+        public ActionResult Delete(int MaDC)
+        {
+            _idocument.Delete(MaDC);
+            return NoContent();
+        }
+        [HttpPut]
+        public ActionResult PutPower(Documentz documentz)
+        {
+            _idocument.Update(documentz);
+            return NoContent();
+        }
+    }
+}
